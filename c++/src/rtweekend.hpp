6a97ca78dc657da7fdbfda55d9d3f63699fe2a2b
jpaquim <joaopaquim@gmail.com>
2020-11-18 01:24:46 +0000

    Add antialiasing

--- c++/src/rtweekend.hpp
+++ c++/src/rtweekend.hpp
@@ -14,6 +14,30 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) { return degrees * pi / 180.; }
 
+inline double random_double() { return rand() / (RAND_MAX + 1.); }
+
+// alternative
+// #include <random>
+// inline double random_double() {
+//     static std::uniform_real_distribution<double> distribution(0., 1.);
+//     static std::mt19937 generator;
+//     return distribution(generator);
+// }
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) {
+        return min;
+    }
+    if (x > max) {
+        return max;
+    }
+    return x;
+}
+
 #include "ray.hpp"
 #include "vec3.hpp"
 
